e514785c667e2dded3451119d77c90b7c60851f2
Nathalia <nathanastacio@gmail.com>
2024-10-09 16:52:12 -0300

    Resolvendo Passo 1

--- codigo.py
+++ codigo.py
@@ -1,4 +1,17 @@
 #Passo 1: Entrar no sistema da empresa - https://dlp.hashtagtreinamentos.com/python/intensivao/login
+import pyautogui
+
+pyautogui.PAUSE = 1.5
+
+pyautogui.press('win')
+pyautogui.write('chrome')
+pyautogui.press('enter')
+
+site = 'https://dlp.hashtagtreinamentos.com/python/intensivao/login'
+pyautogui.write(site)
+pyautogui.press('enter')
+
+
 #Passo 2: Fazer login
 #Passo 3: Importar a base de dados
 #Passo 4: Cadastrar 1 produto
