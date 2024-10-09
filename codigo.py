b533a97a4c7aae9a42f51c3c8e34f9fda83d864e
Nathalia <nathanastacio@gmail.com>
2024-10-09 16:45:11 -0300

    Identificando os passos

--- codigo.py
+++ codigo.py
@@ -1,1 +1,6 @@
+#Passo 1: Entrar no sistema da empresa - https://dlp.hashtagtreinamentos.com/python/intensivao/login
+#Passo 2: Fazer login
+#Passo 3: Importar a base de dados
+#Passo 4: Cadastrar 1 produto
+#Passo 5: Repetir o processo de cadastro até acabar os produtos
 
